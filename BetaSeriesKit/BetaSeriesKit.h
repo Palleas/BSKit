c6e8d20b6e57b5edb15a9ef68e4b56002a3f44b4
Romain Pouclet <rpouclet@mirego.com>
2015-12-19 15:11:27 -0500

    Setup BetaSeriesKit
    * Add two targets
    * Add client class
    * Add Quick and Nimble

---
+++ BetaSeriesKit/BetaSeriesKit.h
@@ -0,0 +1,19 @@
+//
+//  BetaSeriesKit.h
+//  BetaSeriesKit
+//
+//  Created by Romain Pouclet on 2015-12-19.
+//  Copyright © 2015 Perfectly-Cooked. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BetaSeriesKit.
+FOUNDATION_EXPORT double BetaSeriesKitVersionNumber;
+
+//! Project version string for BetaSeriesKit.
+FOUNDATION_EXPORT const unsigned char BetaSeriesKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetaSeriesKit/PublicHeader.h>
+
+
---
+++ BetaSeriesKit/Client.swift
@@ -0,0 +1,13 @@
+//
+//  Client.swift
+//  BetaSeriesKit
+//
+//  Created by Romain Pouclet on 2015-12-19.
+//  Copyright © 2015 Perfectly-Cooked. All rights reserved.
+//
+
+import Foundation
+
+class Client {
+    
+}
