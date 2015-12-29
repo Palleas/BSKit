9b5754e5e340cee93154636d51795af6e89531ff
Romain Pouclet <rpouclet@mirego.com>
2015-12-29 11:03:12 -0500

    Setup Quick & Nimbe client spec

---
+++ BetaSeriesKitTests/ClientSpec.swift
@@ -0,0 +1,41 @@
+//
+//  ClientSpec.swift
+//  BetaSeriesKit
+//
+//  Created by Romain Pouclet on 2015-12-27.
+//  Copyright © 2015 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+import Quick
+import Nimble
+
+class ClientSpec: QuickSpec {
+    override func spec() {
+        describe("Authentication") {
+            it("should open a browser with the authentication page") {
+                
+            }
+            
+            it("should extract the code from the completion URL") {
+                
+            }
+            
+            it("should request a token") {
+            
+            }
+        }
+        
+        describe("User") {
+            it("should fetch a user profile") {
+                
+            }
+        }
+        
+        describe("Episodes") {
+            it("should fetch the episodes list for a TV Show") {
+            
+            }
+        }
+    }
+}
