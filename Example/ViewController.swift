6358c23721d5cc3a661392800051e41cf06e1a83
Romain Pouclet <rpouclet@mirego.com>
2016-03-19 11:32:41 -0400

    Add example app

---
+++ Example/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Romain Pouclet on 2016-02-23.
+//  Copyright © 2016 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+import BetaSeriesKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let client = Client(key: "")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
